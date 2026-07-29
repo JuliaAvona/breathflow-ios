f6e1689f3130ac546b88ab592516293b68e2932b
Julia Avona <uliaavona535@gmail.com>
2026-07-29 09:12:28 -0600

    fix

--- app.config.ts
+++ app.config.ts
@@ -74,7 +74,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: 'BreathFlow — Breathing Exercises',
   slug: 'breathflow',
-  version: '1.2',
+  version: '1.3',
   orientation: 'portrait',
   icon: './assets/icon.png',
   userInterfaceStyle: 'automatic',
@@ -85,7 +85,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ios: {
     supportsTablet: false,
     bundleIdentifier: 'com.izbrodin90.breathflow',
-    buildNumber: '21',
+    buildNumber: '22',
     usesAppleSignIn: true,
     appleTeamId: '9B587AMM75',
     infoPlist: {
